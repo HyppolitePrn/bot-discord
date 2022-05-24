550bd169f7d1fa560b909672d798f61dc80d7b61
Thierry Maignan <maignan.thierry@outlook.fr>
2022-05-24 19:52:43 +0200

    feat: get_current_node

--- Bot_discord2.py
+++ Bot_discord2.py
@@ -1,18 +1,21 @@
 from Bot_discord3 import Node
 
-root = Node("A","B")
+root = Node("A", "B")
 
 
-root.insert_node(Node("B","C"), "A")
-root.insert_node(Node("H","V"), "A")
-root.insert_node(Node("F","D"), "B")
-root.insert_node(Node("tg","pd"), "B")
-root.insert_node(Node("hello","salut"), "F")
+root.insert_node(Node("B", "C"), "A")
+root.insert_node(Node("H", "V"), "A")
+root.insert_node(Node("F", "D"), "B")
+root.insert_node(Node("tg", "pd"), "B")
+root.insert_node(Node("hello", "salut"), "F")
+root.insert_node(Node("maignan", "thierry"), "tg")
+root.insert_node(Node("popo", "hippo"), "tg")
+root.insert_node(Node("cala", "cam"), "popo")
 current_node = root
 keyword = input()
 
 
-def get_current_node(current_node,keyword):
+def get_current_node(current_node, keyword):
 
     print(current_node.question)
     for Node in current_node.list_child_node:
@@ -22,11 +25,29 @@ def get_current_node(current_node,keyword):
         else:
             if len(Node.list_child_node) > 0:
                 for Node in Node.list_child_node:
-                        # print(Node.question)
-                        get_current_node(Node, keyword)
+                    # print(Node.question)
+                    get_current_node(Node, keyword)
+
+# ça marche a moitier lui tu peux check hippo
 
 
-print(get_current_node(current_node,keyword))
+def get_current_node_2(current_node, keyword):
+    for Node in current_node.list_child_node:
+
+        if keyword == Node.keyword:
+            current_node = Node.question
+            return current_node
+        else:
+            for Node in Node.list_child_node:
+                if len(Node.list_child_node) > 0:
+                    print(Node.question, 'Q')
+                    match = get_current_node_2(Node, keyword)
+                    print(match, 'match')
+                    if match:
+                        return match
+
+
+print(get_current_node_2(current_node, keyword))
 # if keyword == keyword:
 #     current_node = question
 #     current_node
@@ -35,4 +56,3 @@ print(get_current_node(current_node,keyword))
 #         if child_node.keyword == keyword:
 #             current_node = child_node.question
 #             return current_node
-
